a658e4e33923cae3d10c7c617c056d4094cd849e
Simonwol110 <swl@silvi.ai>
2022-01-05 11:49:45 +0100

    Finished the matmult_lib wrapper

--- matmult_lib.c
+++ matmult_lib.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 #include <gsl/gsl_cblas.h>
 
-void matmult_lib(int m, int n, int k, doulbe **A, double **B, double **C)
+void matmult_lib(int m, int n, int k, double **A, double **B, double **C)
 {
     double alpha = 1.0;
     double beta = 0.0;
@@ -11,5 +10,3 @@ void matmult_lib(int m, int n, int k, doulbe **A, double **B, double **C)
                     CblasNoTrans, CblasNoTrans, m, n, k,
                     alpha, A, k, B, n, beta, C, n);        
 }
-
-// gcc -Wall -I/home/simonl/gsl/include -c main_2.c; gcc -L/home/silas/gsl/lib main_2.o -lgsl -lgslcblas -lm -o main_2; ./main_2;
