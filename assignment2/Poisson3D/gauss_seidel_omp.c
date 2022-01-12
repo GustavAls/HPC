a4c9ff5653bbce07f9f7b4fb0a2bf54a49816395
GustavAls <gustavals1998@gmail.com>
2022-01-12 18:31:27 +0100

    4

--- assignment2/Poisson3D/gauss_seidel_omp.c
+++ assignment2/Poisson3D/gauss_seidel_omp.c
@@ -18,7 +18,7 @@ gauss_seidel(double ***u,double ***F,int N, int iterations, double tolerance) {
     for (n = 0; n < iterations; n++){
         dist = 0;
         //Default schedule would be (static, N/P), with N work and P threads
-        #pragma omp for ordered schedule(static, 1)
+        #pragma omp for ordered(2) schedule(static, 1) default(shared)
         for(int i = 1; i < (N - 1); i++){
             for(int j = 1; j < (N - 1); j++){
             #pragma omp ordered \
@@ -35,7 +35,7 @@ gauss_seidel(double ***u,double ***F,int N, int iterations, double tolerance) {
                     dist += (u[i][j][k] - old_u)*(u[i][j][k] - old_u);
                 }
             #pragma omp ordered depend(source) /*Ending ordered*/
-            }
+        }
         }/*End of for ordered*/
         dist = sqrt(dist);
         n++;
