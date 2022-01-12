abcb5389841a3f89578d2768ba0615b420054fde
GustavAls <gustavals1998@gmail.com>
2022-01-12 16:50:37 +0100

    header change

--- assignment2/Poisson3D/gauss_seidel_omp.h
+++ assignment2/Poisson3D/gauss_seidel_omp.h
@@ -1,8 +1,8 @@
 /* gauss_seidel.h - Poisson problem
  *
  */
-#ifndef _GAUSS_SEIDEL_OMP_H
-#define _GAUSS_SEIDEL_OMP_H
+#ifndef _GAUSS_SEIDEL_H
+#define _GAUSS_SEIDEL_H
 
 // define your function prototype here
 int gauss_seidel_omp(double ***u,double ***F, int N, int iterations, double tolerance);
