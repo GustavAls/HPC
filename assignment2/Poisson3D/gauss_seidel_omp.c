94f138210cfc7e3f9134c5b7b02833e238ba4a70
GustavAls <gustavals1998@gmail.com>
2022-01-12 18:43:50 +0100

    brackets

--- assignment2/Poisson3D/gauss_seidel_omp.c
+++ assignment2/Poisson3D/gauss_seidel_omp.c
@@ -13,28 +13,30 @@ gauss_seidel(double ***u,double ***F,int N, int iterations, double tolerance) {
     double old_u;
     double dist;
     dist = tolerance + 1.0;
-    #pragma omp parallel private(n)
+    #pragma omp parallel private(n) default(shared)
     {
     for (n = 0; n < iterations; n++){
         dist = 0;
         //Default schedule would be (static, N/P), with N work and P threads
-        #pragma omp for default(shared) schedule(static, 1) ordered(2) 
-        for(int i = 1; i < (N - 1); i++){
-            for(int j = 1; j < (N - 1); j++){
-            #pragma omp ordered \
-                    depend(sink:i-1, j) \
-                    depend(sink:i, j-1)
-                for(int k = 1; k < (N - 1); k++){
-                    old_u = u[i][j][k];
-                    u[i][j][k] = 1.0 / 6.0*(
-                        u[i-1][j][k] + u[i+1][j][k] + 
-                        u[i][j-1][k] + u[i][j+1][k] + 
-                        u[i][j][k-1] + u[i][j][k+1] + 
-                        delta2 * F[i][j][k]
-                    );
-                    dist += (u[i][j][k] - old_u)*(u[i][j][k] - old_u);
+        #pragma omp for ordered(2) schedule(dynamic)//schedule(static, 1) 
+        {
+            for(int i = 1; i < (N - 1); i++){
+                for(int j = 1; j < (N - 1); j++){
+                #pragma omp ordered depend(sink:i-1,j) depend(sink:i,j-1)
+                {
+                    for(int k = 1; k < (N - 1); k++){
+                        old_u = u[i][j][k];
+                        u[i][j][k] = 1.0 / 6.0*(
+                            u[i-1][j][k] + u[i+1][j][k] + 
+                            u[i][j-1][k] + u[i][j+1][k] + 
+                            u[i][j][k-1] + u[i][j][k+1] + 
+                            delta2 * F[i][j][k]
+                        );
+                        dist += (u[i][j][k] - old_u)*(u[i][j][k] - old_u);
+                    }
+                #pragma omp ordered depend(source) /*Ending ordered*/
                 }
-            #pragma omp ordered depend(source) /*Ending ordered*/
+            }
         }
         }/*End of for ordered*/
         dist = sqrt(dist);
