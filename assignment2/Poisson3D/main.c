842f240df9cd5955a70b9c4b42646e1365f96a1e
Silas Brack <s174433@student.dtu.dk>
2022-01-12 10:30:41 +0100

    Implemented Gauss-Seidel.

--- assignment2/Poisson3D/main.c
+++ assignment2/Poisson3D/main.c
@@ -3,6 +3,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 #include "alloc3d.h"
 #include "print.h"
 
@@ -16,6 +17,24 @@
 
 #define N_DEFAULT 100
 
+double calculate_f(int N, int i, int j, int k){
+    double delta = 2.0/((double)N-1.0);
+    double x = delta*i - 1.0;
+    double y = delta*j - 1.0;
+    double z = delta*k - 1.0;
+
+    if( -1.0<=x && x<=-3.0/8.0 ){
+        if( -1.0<=y && y<=-1.0/2.0){
+            if( -2.0/3.0<=z && z<=0.0){
+                return(200.0);
+            }
+        }
+    }
+    else{
+    return(0.0);
+    }
+}
+
 int
 main(int argc, char *argv[]) {
 
@@ -28,6 +47,7 @@ main(int argc, char *argv[]) {
     char        *output_ext    = "";
     char	output_filename[FILENAME_MAX];
     double 	***u = NULL;
+    double 	***F = NULL;
 
 
     /* get the paramters from the command line */
@@ -36,7 +56,7 @@ main(int argc, char *argv[]) {
     tolerance = atof(argv[3]);  // tolerance
     start_T   = atof(argv[4]);  // start T for all inner grid points
     if (argc == 6) {
-	output_type = atoi(argv[5]);  // ouput type
+	    output_type = atoi(argv[5]);  // ouput type
     }
 
     // allocate memory
@@ -44,13 +64,59 @@ main(int argc, char *argv[]) {
         perror("array u: allocation failed");
         exit(-1);
     }
+    if ( (F = d_malloc_3d(N, N, N)) == NULL ) {
+        perror("array F: allocation failed");
+        exit(-1);
+    }
 
-    /*
-     *
-     * fill in your code here 
-     *
-     *
-     */
+    // This is hideous, maybe should put it in a separate file? Idk
+    for(int i=0;i<(N);i++){
+        for(int k=0;k<(N);k++){
+            // u_old[i][0][k] = 0;
+            u[i][0][k] = 0;
+
+            // u_old[i][N-1][k] = 20;
+            u[i][N-1][k] = 20;
+        }
+        for(int j=0; j<N; j++){
+            // u_old[i][j][0] = 20;
+            u[i][j][0] = 20;
+
+            // u_old[i][j][N-1] = 20;
+            u[i][j][N-1] = 20;
+        }
+    }
+    for(int j=0; j<N; j++){
+        for(int k=0; k<N; k++){
+            // u_old[0][j][k] = 20;
+            u[0][j][k] = 20;
+
+            // u_old[N-1][j][k] = 20;
+            u[N-1][j][k] = 20;
+        }
+    }
+    //Interior points
+    for(int i=1;i<(N-1);i++){
+        for(int j=1;j<(N-1);j++){
+            for(int k=1;k<(N-1);k++){
+                // u_old[i][j][k] = start_T;
+                u[i][j][k] = start_T;
+                F[i][j][k] = calculate_f(i,j,k,N);
+            }
+        }
+    }
+
+    double start, elapsed;
+    int iter;
+    #ifdef _GAUSS_SEIDEL
+    start = omp_get_wtime();
+    iter = gauss_seidel(u, F, N, iter_max, tolerance);
+    elapsed = omp_get_wtime() - start;
+    #endif
+
+    // FILE *file = fopen("result.txt", "w");
+    // fprintf(file, "%f %d", elapsed, iter);
+    printf("%d %f %d \n", N, elapsed, iter);
 
     // dump  results if wanted 
     switch(output_type) {
