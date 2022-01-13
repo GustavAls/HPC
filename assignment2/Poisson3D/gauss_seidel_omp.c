94c22aeefea3ab1746022611116de8e2513c589c
GustavAls <gustavals1998@gmail.com>
2022-01-13 12:41:36 +0100

    4

--- assignment2/Poisson3D/gauss_seidel_omp.c
+++ assignment2/Poisson3D/gauss_seidel_omp.c
@@ -11,34 +11,27 @@ gauss_seidel_omp(double ***u,double ***F,int N, int iterations, double tolerance
     int i, j, k;
     double delta = 2.0/((double)N-1.0);
     double delta2 = delta*delta;
-    double old_u;
-    double dist;
     double div = 1.0/6.0;
-    dist = tolerance + 1.0;
     #pragma omp parallel default(none) private(n) \
-         shared(delta2, dist, old_u, u, N, tolerance, F, iterations, div)
+         shared(delta2, u, N, tolerance, F, iterations, div)
     {
     for (n = 0; n < iterations; n++){
-        //dist = 0;
         //Default schedule would be (static, N/P), with N work and P threads
         #pragma omp for private(i,j,k) ordered(2) schedule(static,1)
         for(i = 1; i < (N - 1); i++){
             for(j = 1; j < (N - 1); j++){
                 #pragma omp ordered depend(sink:i-1,j) depend(sink:i,j-1)
                 for(k = 1; k < (N - 1); k++){
-                    old_u = u[i][j][k];
                     u[i][j][k] = div * (
                         u[i-1][j][k] + u[i+1][j][k] + 
                         u[i][j-1][k] + u[i][j+1][k] + 
                         u[i][j][k-1] + u[i][j][k+1] + 
                         delta2 * F[i][j][k]
                     );
-                    //dist += (u[i][j][k] - old_u)*(u[i][j][k] - old_u);
                     }
                 #pragma omp ordered depend(source) /*Ending ordered*/
             }
         }/*End of for ordered*/
-        //dist = sqrt(dist);
     }
     }/*End of parallel*/
     return(n);
