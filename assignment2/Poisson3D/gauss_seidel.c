a56d6e53163250c126074e60853764f9ee133c52
DeterjoSimon <simon-dk@hotmail.fr>
2022-01-13 11:04:10 +0100

    Added funroll yay! And refactored a small computation

--- assignment2/Poisson3D/gauss_seidel.c
+++ assignment2/Poisson3D/gauss_seidel.c
@@ -10,6 +10,7 @@ gauss_seidel(double ***u,double ***F,int N, int iterations, double tolerance) {
     double delta = 2.0/((double)N-1.0);
     double delta2 = delta*delta;
     double old_u;
+    double factor = 1.0 / 6.0;
     double dist;
     dist = tolerance + 1.0;
     n = 0;
@@ -19,7 +20,7 @@ gauss_seidel(double ***u,double ***F,int N, int iterations, double tolerance) {
             for(int j = 1; j < (N - 1); j++){
                 for(int k = 1; k < (N - 1); k++){
                     old_u = u[i][j][k];
-                    u[i][j][k] = 1.0/6.0*(
+                    u[i][j][k] = factor*(
                         u[i-1][j][k] + u[i+1][j][k] + 
                         u[i][j-1][k] + u[i][j+1][k] + 
                         u[i][j][k-1] + u[i][j][k+1] + 
