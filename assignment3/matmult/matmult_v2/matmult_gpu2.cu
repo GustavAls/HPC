280aeb0f3834640450c65c135bb510c96b4357d9
GustavAls <gustavals1998@gmail.com>
2022-01-19 15:16:17 +0100

    cast

--- assignment3/matmult/matmult_v2/matmult_gpu2.cu
+++ assignment3/matmult/matmult_v2/matmult_gpu2.cu
@@ -23,8 +23,6 @@ __global__ void kernel2(int m, int n, int k, double *A, double *B, double *C) {
 extern "C" {
     void matmult_gpu2(int m, int n, int k, double *A, double *B, double *C) {
         double* A_d, * B_d, * C_d;
-        int d1,d2;
-        int block_size = 16;
         //Cuda allocate memory on device for matrices
         cudaMalloc((void**)&A_d, m*k * sizeof(double));
         cudaMalloc((void**)&B_d, k*n * sizeof(double));
@@ -36,14 +34,18 @@ extern "C" {
 
         //Initialize zeros in the output matrix
         cudaMemset(C_d, 0, m*n*sizeof(double));
+
+        int d1,d2;
+        int block_size = 16;
                 
         //Assigning a 2D thread grid in each block
         dim3 threadsPerBlock(block_size, block_size);
 
         //Defining the grid layout depending on the input dimensions
-        d1 = (n - 1) / block_size + 1;
-        d2 = (m - 1) / block_size + 1;
+        d1 = (int) (n - 1) / block_size + 1;
+        d2 = (int) (m - 1) / block_size + 1;
 
+        //Defining grid size
         dim3 blocksPerGrid(d1, d2);
 
         kernel2<<<blocksPerGrid,threadsPerBlock>>>(m, n, k, A_d, B_d, C_d);
