a452eec81485005d680a994d1c4bb4d20eba0ca1
GustavAls <gustavals1998@gmail.com>
2022-01-19 14:07:48 +0100

    matmult_gpu2.cu

--- assignment3/matmult/matmult_v1/matmult_lib.cu
+++ assignment3/matmult/matmult_v1/matmult_lib.cu
@@ -5,7 +5,7 @@
 //Identical to week 1 version, however now using single pointer
 extern "C" {
 	void matmult_lib(int m, int n, int k, double *A, double *B, double *C){
-		double alpha = 1.0
+		double alpha = 1.0;
         double beta = 0.0;
 		cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
               		m, n, k, alpha, A, k, B, n, beta, C, n);
