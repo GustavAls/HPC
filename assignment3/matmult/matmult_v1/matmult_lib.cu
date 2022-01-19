8c0589fd126a7516f5df59912d1cf574f0a41ed9
DeterjoSimon <simon-dk@hotmail.fr>
2022-01-19 14:27:24 +0100

    Included the header file for the externalisation of the script

--- assignment3/matmult/matmult_v1/matmult_lib.cu
+++ assignment3/matmult/matmult_v1/matmult_lib.cu
@@ -4,7 +4,7 @@
 //Identical to week 1 version, however now using single pointer
 extern "C" {
 	#include <cblas.h>
-	void matmult_lib(int m, int n, int k, double *A, double *B, double *C){
+	void matmult_lib(int m, int	 n, int k, double *A, double *B, double *C){
 		double alpha = 1.0;
         double beta = 0.0;
 		cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
